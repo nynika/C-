67f5d68beb336c6a113be963115fbc7759885b5f
Sujithra <122441546+nynika@users.noreply.github.com>
2024-07-02 12:41:19 +0530

    Add files via upload

---
+++ RIMC_WEBAPI/System/Web/HttpContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace System.Web
+{
+    internal class HttpContext
+  
+    {
+        public static HttpContext Current { get; set; }
+        public HttpRequest Request { get; }
+
+    }
+}
